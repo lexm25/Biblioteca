66d64a5b3499ee0e3d809db20bbe1912a07a46ce
Tu Nombre <Eduardo Serrano Luque>
2021-01-27 15:05:34 +0100

    clase 3

---
+++ src/main/java/clases/Libro.java
@@ -0,0 +1,87 @@
+package clases;
+
+public class Libro {
+	
+	private String titulo;
+	private String isbn;
+	private Genero genero;
+	private String autor;
+	private Integer paginas;
+	
+	public Libro() {
+		
+	}
+
+	/**
+	 * @return the titulo
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+
+	/**
+	 * @param titulo the titulo to set
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	/**
+	 * @return the isbn
+	 */
+	public String getIsbn() {
+		return isbn;
+	}
+
+	/**
+	 * @param isbn the isbn to set
+	 */
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	/**
+	 * @return the genero
+	 */
+	public Genero getGenero() {
+		return genero;
+	}
+
+	/**
+	 * @param genero the genero to set
+	 */
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	/**
+	 * @return the autor
+	 */
+	public String getAutor() {
+		return autor;
+	}
+
+	/**
+	 * @param autor the autor to set
+	 */
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	/**
+	 * @return the paginas
+	 */
+	public Integer getPaginas() {
+		return paginas;
+	}
+
+	/**
+	 * @param paginas the paginas to set
+	 */
+	public void setPaginas(Integer paginas) {
+		this.paginas = paginas;
+	}
+	
+	
+
+}
--- src/main/java/clases/Main.java
+++ src/main/java/clases/Main.java
@@ -3,9 +3,113 @@
  */
 package clases;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     
     public static void main (String [] args) {
-    	System.out.println("Hola Mundo");
+    	
+    	ArrayList<Libro> catalogo = new ArrayList<Libro>();
+    	
+    	while(true) {
+    		int opcion = menu();
+    		switch (opcion) {
+			case 1:
+				//TODO Alta de Libro
+				//titulo:isbn:genero:autor:paginas
+				alta(catalogo);
+				break;
+			case 2:
+				//TODO Lista de Libros
+				break;
+			default:
+				break;
+			}
+    	}
+    }
+    
+    private static int menu() {
+    	int opcion=0;
+    	
+    	do {
+    		System.out.println("Opciones:");
+    		System.out.println("1. Alta de Libro");
+    		System.out.println("2. Lista de Libros");
+    		System.out.println("Introduce la opcion:");
+    	
+    		opcion = leerOpcion(2);
+    		
+    	}while(opcion <=0);
+    	
+    	return opcion;
+    }
+    
+    private static int leerOpcion(int max) {
+    	int opcion = -1;
+    	try {
+    		Scanner teclado = new Scanner(System.in);
+    		opcion=teclado.nextInt();
+    		if(opcion>max)
+    			opcion=-1;
+    	}catch (InputMismatchException e) {
+			System.out.println("Opción incorrecta");
+		}
+    	
+    	return opcion;
+    }
+    
+    private static void alta(ArrayList<Libro> catalogo) {
+    	//Leer de la entrada
+    	String datosLibro = obtenerDatosLibro();
+    	//titulo:isbn:genero:autor:paginas
+    	//Procesar la entrada
+    	procesaEntrada(datosLibro);
+    	//Crear el libro con los datos de la entrada
+    	//Meer el libro en el catalogo
+    }
+    
+    private static String obtenerDatosLibro() {
+    	String datos=null;
+    	
+    	boolean validado=false;
+    	while(!validado) {
+    		System.out.println("Introduce los datos de un libro.");
+    		System.out.println("Usa el formato \"titulo:isbn:genero:autor:paginas \"");
+    		try {
+    			datos = leerCadena();
+    			if(true)
+    				validado=true;
+    		}catch (InputMismatchException e) {
+				System.out.println("Datos de entrada no válidos");
+			}
+    	}
+    	
+    	return datos;
+    }
+    
+    private static Libro procesaEntrada(String entrada) {
+    	Libro libro = null;
+    	
+    	String [] datos = entrada.split(":");
+    	
+    	String titulo = datos[0];
+    	String isbn = datos[1];
+    	Genero genero = Genero.getGenero(datos[2]);
+    	String autor = datos[3];
+    	Integer paginas = Integer.parseInt(datos[4]);
+    	
+    	//Constructor por defecto
+    	
+    	return libro;
+    }
+    
+    private static String leerCadena() {
+    	String opcion = null;
+    	Scanner teclado = new Scanner(System.in);
+        opcion=teclado.nextLine();
+    	return opcion;
     }
 }
